a426851d8d093e4c5c3576e21d7886597e6202d3
Andrey Alekseev <ilekseev@gmail.com>
2020-02-19 01:59:39 +0300

    FIX: GaborConv2d vectorised

--- GaborNet/GaborLayers.py
+++ GaborNet/GaborLayers.py
@@ -8,15 +8,13 @@ from torch.nn import functional as F
 class GaborConv2d(nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size):
-
         super().__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
 
-        self.register_parameter('weight', nn.Parameter(
-            torch.empty(out_channels, in_channels, kernel_size[0], kernel_size[1], requires_grad=False)))
+        self.weight = torch.empty(out_channels, in_channels, kernel_size[0], kernel_size[1], requires_grad=False)
         self.register_parameter('freq', nn.Parameter(
             (3.14 / 2) * 1.41 ** (-torch.randint(0, 5, (out_channels, in_channels))).type(torch.Tensor)))
         self.register_parameter('theta', nn.Parameter(
@@ -28,22 +26,18 @@ class GaborConv2d(nn.Module):
         y0 = torch.ceil(torch.Tensor([self.kernel_size[1] / 2]))[0]
         y, x = torch.meshgrid([torch.linspace(-x0 + 1, x0, kernel_size[0]),
                                torch.linspace(-y0 + 1, y0, kernel_size[1])])
-        self.y = nn.Parameter(y, requires_grad=False)
-        self.x = nn.Parameter(x, requires_grad=False)
+        self.y = nn.Parameter(y.unsqueeze(0).unsqueeze(0).expand_as(self.weight), requires_grad=False)
+        self.x = nn.Parameter(x.unsqueeze(0).unsqueeze(0).expand_as(self.weight), requires_grad=False)
 
     def forward(self, input_image):
-        for i in range(self.out_channels):
-            for j in range(self.in_channels):
-                sigma = self.sigma[i, j].expand_as(self.y)
-                freq = self.freq[i, j].expand_as(self.y)
-                theta = self.theta[i, j].expand_as(self.y)
-                psi = self.psi[i, j].expand_as(self.y)
-
-                rotx = self.x * torch.cos(theta) + self.y * torch.sin(theta)
-                roty = -self.x * torch.sin(theta) + self.y * torch.cos(theta)
-
-                g = torch.exp(-0.5 * ((rotx ** 2 + roty ** 2) / (sigma + 1e-3) ** 2))
-                g = g * torch.cos(freq * rotx + psi)
-                g = g / (2 * 3.14 * sigma ** 2)
-                self.weight[i, j] = g
+        sigma = self.sigma.unsqueeze(-1).unsqueeze(-1).expand_as(self.weight)
+        freq = self.freq.unsqueeze(-1).unsqueeze(-1).expand_as(self.weight)
+        theta = self.theta.unsqueeze(-1).unsqueeze(-1).expand_as(self.weight)
+        psi = self.psi.unsqueeze(-1).unsqueeze(-1).expand_as(self.weight)
+        rotx = self.x * torch.cos(theta) + self.y * torch.sin(theta)
+        roty = -self.x * torch.sin(theta) + self.y * torch.cos(theta)
+        g = torch.exp(-0.5 * ((rotx ** 2 + roty ** 2) / (sigma + 1e-3) ** 2))
+        g = g * torch.cos(freq * rotx + psi)
+        g = g / (2 * 3.14 * sigma ** 2)
+        self.weight = g
         return F.conv2d(input_image, self.weight)
